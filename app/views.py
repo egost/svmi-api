4438295f6ccdec48aca73ab9a7a84fc3fa19a239
Eu <emej92@gmail.com>
2018-10-24 14:14:51 -0400

    Changed port settings for heroku

--- app/views.py
+++ app/views.py
@@ -24,4 +24,6 @@ def telemetry(payload):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    debug = str(os.environ.get('DEBUG', False))
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=debug)
