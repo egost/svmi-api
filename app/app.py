26223bebce8cccaf291602aaa9eb280081049d8d
Eu <emej92@gmail.com>
2018-11-03 13:46:17 -0400

    SVMI models, initial GET and POST

--- app/app.py
+++ app/app.py
@@ -1,28 +1,34 @@
 import os
-import datetime
+
+import requests
 
 from flask_api import FlaskAPI
 from flask_api import status, exceptions
 from flask import request, url_for, jsonify, make_response
-from flask import Blueprint
-from sqlalchemy.orm import lazyload, raiseload, relationship
 
 from flask_sqlalchemy import SQLAlchemy
 
+# TODO: Handle nested object POST (Address or Industry)
+# TODO: Add single object GET by id
+# TODO: Add single object DELETE by id
+# TODO: Handle existing object POST Address, School and Company (Industry is taken care of)
 
 ##########
 # Models #
 ##########
 
-from app.factory import make_player, make_school, make_session, make_round
 from app.extensions import db
-from app.models import Address, Login, School, Contact, Classroom, Player, Game, Session, Round, System
+from app.models import Address, School, Industry, Company
+
 
 ############
 # Start Up #
 ############
 
 app = FlaskAPI(__name__)
+# TODO: Setup SERVER_NAME appropiately
+# app.config['SERVER_NAME'] = str(os.environ.get('SERVER_NAME', 'http://localhost'))
+
 DB_NAME = str(os.environ.get('DB_NAME'))
 DB_USER = str(os.environ.get('DB_USER'))
 DB_PASSWORD = str(os.environ.get('DB_PASSWORD'))
@@ -36,258 +42,137 @@ db.app = app
 db.init_app(app)
 
 def main():
-    # db.create_all()
-
     debug = str(os.environ.get('DEBUG', True))
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=debug)
-
 
 
 ##########
 # Routes #
 ##########
 
-def make_system(data):
-    # TODO: Handle if no system
-    # if player.system is None:
-    #     print('Player has no preexisting system information.')
-
-    system = System(
-        deviceId = data['deviceUniqueIdentifier'],
-        deviceModel = data['deviceModel'],
-        operatingSystem = data['operatingSystem'],
-        graphicsDeviceVendorId = data['graphicsDeviceVendorId'],
-        graphicsDeviceId = data['graphicsDeviceId'],
-        graphicsDeviceVersion = data['graphicsDeviceVersion'],
-        graphicsMultiThreaded = data['graphicsMultiThreaded'],
-        graphicsShaderLevel = data['graphicsShaderLevel'],
-        maxTextureSize = data['maxTextureSize'],
-        systemMemorySize = data['systemMemorySize'],
-        graphicsMemorySize = data['graphicsMemorySize'],
-        graphicsDeviceVendor = data['graphicsDeviceVendor'],
-        processorCount = data['processorCount'],
-        processorType = data['processorType'],
-        supportedRenderTargetCount = data['supportedRenderTargetCount'],
-        supports2DArrayTextures = data['supports2DArrayTextures'],
-        supports3DRenderTextures = data['supports3DRenderTextures'],
-        supports3DTextures = data['supports3DTextures'],
-        supportsComputeShaders = data['supportsComputeShaders'],
-        supportsInstancing = data['supportsInstancing']
-    )
-
-    return system
 
 
-def existing_player(data):
-    # TODO: Get all player data
-    # skillLevel
-    player = Player.query.filter_by(name=data['playerName']).first()
-    if player is None:
-        player = Player(
-            name=data['playerName'],
-        )
-    player.levels_unlocked=int(data['levelsUnlocked'])
-    return player
-
-def existing_system(data):
-    system = System.query.filter_by(deviceId=data['deviceUniqueIdentifier']).first()
-    if system is None:
-        system = System(
-            deviceId = data['deviceUniqueIdentifier'],
-            deviceModel = data['deviceModel'],
-            operatingSystem = data['operatingSystem'],
-            graphicsDeviceVendorId = data['graphicsDeviceVendorId'],
-            graphicsDeviceId = data['graphicsDeviceId'],
-            graphicsDeviceVersion = data['graphicsDeviceVersion'],
-            graphicsMultiThreaded = data['graphicsMultiThreaded'],
-            graphicsShaderLevel = data['graphicsShaderLevel'],
-            maxTextureSize = data['maxTextureSize'],
-            systemMemorySize = data['systemMemorySize'],
-            graphicsMemorySize = data['graphicsMemorySize'],
-            graphicsDeviceVendor = data['graphicsDeviceVendor'],
-            processorCount = data['processorCount'],
-            processorType = data['processorType'],
-            supportedRenderTargetCount = data['supportedRenderTargetCount'],
-            supports2DArrayTextures = data['supports2DArrayTextures'],
-            supports3DRenderTextures = data['supports3DRenderTextures'],
-            supports3DTextures = data['supports3DTextures'],
-            supportsComputeShaders = data['supportsComputeShaders'],
-            supportsInstancing = data['supportsInstancing']
-        )
-    return system
-
-def assemble_session(data):
-    """ Assembles session object """
-
-    import pprint
-    pprint.pprint(data)
-
-    # Schema objects
-    player = existing_player(data)
-    system = existing_system(data)
-
-    stop_time = datetime.datetime.now()
-    seconds = round(float(data['stopTime']))
-    start_time = stop_time - datetime.timedelta(seconds)
-
-    session = Session(
-        #length=datetime.time(datetime.datetime.fromtimestamp(round(float(data['stopTime'])))),
-        start_time=start_time,
-        stop_time=stop_time,
-        player=player,
-        system=system
-    )
-
-    db.session.add(session)
-    db.session.commit()
-
-def existing_session(data):
-    # TODO: check/create existing session
-    pass
-
-def assemble_round(data):
-    """ Assembles round object """
-
-    import pprint
-    pprint.pprint(data)
-
-    # Schema objects
-    # session = existing_session(data)
-
-    stop_time = datetime.datetime.now()
-    seconds = round(float(data['stopTime']))
-    start_time = stop_time - datetime.timedelta(seconds)
-
-    _round = Round(
-        start_time=start_time,
-        stop_time=stop_time,
-        score=data['score']
-    )
-
-    db.session.add(_round)
-    db.session.commit()
-
-# Game Telemetry
-@app.route('/api/session', methods=['POST'])
-def session_telemetry():
+# Address
+@app.route('/api/address', methods=['GET', 'POST'])
+def addresses():
     '''
-    GETS or POSTS telemetry    
+    GET: Lists all addresses.
+    POST: Creates an address.
     '''
+
     if request.method == 'POST':
-        # print('Data:', request.json.get('data', ''))
-        # print('JSON:', request.get_json())
-        assemble_session(request.get_json())
-        # print(request.get_json()['data'])
-        return 'Successful POST', status.HTTP_201_CREATED
+        json = request.get_json()
 
-@app.route('/api/round', methods=['POST'])
-def round_telemetry():
+        address = Address(
+            street1=json.get('street1', ''),
+            street2=json.get('street2', ''),
+            city=json.get('city', ''),
+            state=json.get('state', ''),
+            zipcode=json.get('zipcode', ''),
+            county=json.get('county', ''),
+        )
+
+        db.session.add(address)
+        db.session.commit()
+
+        return jsonify(address.json), status.HTTP_201_CREATED
+
+    addresses = Address.query.all()
+    return jsonify({'addresses': unpack_json(addresses)}), status.HTTP_200_OK
+
+
+
+# Industry
+@app.route('/api/industry', methods=['GET', 'POST'])
+def industries():
     '''
-    GETS or POSTS telemetry    
+    GET: Lists all industries.
+    POST: Creates an industry.
     '''
+
     if request.method == 'POST':
-        # print('Data:', request.json.get('data', ''))
-        # print('JSON:', request.get_json())
-        assemble_round(request.get_json())
-        # print(request.get_json()['data'])
-        return 'Successful POST', status.HTTP_201_CREATED
+        json = request.get_json()
 
+        industry = Industry.query.filter_by(naics_code=json['naics_code']).first()
+        if industry is None:
+            industry = Industry(
+                naics_code=json.get('naics_code', ''),
+                description=json.get('description', ''),
+            )
+
+            db.session.add(industry)
+            db.session.commit()
+            return jsonify(industry.json), status.HTTP_201_CREATED
+        else:
+            return jsonify(industry.json), status.HTTP_202_ACCEPTED
+
+    industries = Industry.query.all()
+    return jsonify({'industries': unpack_json(industries)}), status.HTTP_200_OK
+
+
+# Company
+@app.route('/api/company', methods=['GET', 'POST'])
+def companies():
+    '''
+    GET: Lists all companies.
+    POST: Creates an company.
+    '''
+
+    if request.method == 'POST':
+        json = request.get_json()
+
+        # with app.test_request_context():
+        #    address = url_for('addresses', json=json['address'])
+           # industry = url_for('industries', json=json['industry'])
+
+        company = Company(
+            name=json.get('name', ''),
+            # address=address,
+            # industry=industry,
+            website=json.get('website', '')
+        )
+
+        db.session.add(company)
+        db.session.commit()
+
+        return jsonify(company.json), status.HTTP_201_CREATED
+
+    companies = Industry.query.all()
+    return jsonify({'companies': unpack_json(companies)}), status.HTTP_200_OK
 
 # School
 @app.route('/api/school', methods=['GET', 'POST'])
 def schools():
     '''
     GET: Lists all schools.
-    POST: Creates a school.
+    POST: Creates an school.
     '''
-    schools = School.query.all()
 
-    # request.method == 'GET'
-    return jsonify({'schools': schools})
-
-
-@app.route('/api/school/<string:school_name>', methods=['GET', 'PUT', 'DELETE'])
-def school(school_name):
-    '''
-    GET: View school information and players.
-    PUT: Not implemented.
-    DELETE: Not implemented.
-    '''
-    school = School.query.filter_by(name=school_name).first()
-    contacts = Contact.query.filter_by(school_id=school.id).all()
-    players = [ Player.query.filter_by(contact=contact).first().json for contact in contacts ]
-
-    # request.method == 'GET'
-    return jsonify(json_list = players)
-
-
-@app.route('/api/school/<string:school_name>/highscores', methods=['GET'])
-def school_highscores(school_name):
-    '''
-    GET: View high scores.
-    '''
-    school = School.query.filter_by(name=name).first()
-    contacts = Contact.query.filter_by(school_id=school.id).all()
-    players = [ Player.query.filter_by(contact=contact).first() for contact in contacts ]
-    # TODO: Get highscores from nested query
-    for player in players:
-        sessions = Session.query.filter_by(player_id=player.id).all()
-
-    # request.method == 'GET'
-    return jsonify({'players':players})
-
-
-# Player
-@app.route('/api/player', methods=['GET', 'POST'])
-def players():
-    '''
-    GET: Lists all players.
-    POST: Creates player.
-    '''
     if request.method == 'POST':
-        # print('Data:', request.json.get('data', ''))
-        # print('JSON:', request.get_json())
+        json = request.get_json()
 
-        school_name = 'Prep Time Education'
+        # with app.test_request_context():
+        #    address = url_for('addresses', json=json['address'])
 
-        school = School.query.filter_by(name=school_name).first()
+        school = School(
+            name=json.get('name', ''),
+            # address=address,
+            website=json.get('website', '')
+        )
 
-        if school is None:
-            school = make_school(name=school_name)
-
-        player = make_player(n_sessions=10)
-        player.contact.school = school
-
-        sessions = [ make_session(player=player) for i in range(0,10) ]
-        rounds = [ make_round(session=session) for i in range(0,3) ]
-        db.session.add(player)
+        db.session.add(school)
         db.session.commit()
 
-        return jsonify(str(player)), status.HTTP_201_CREATED
-
-    # request.method == 'GET'
-    return jsonify(json_list = [ str(player) for player in Player.query.all() ])
-
-
-# Player Session
-@app.route('/api/player/<string:name>/session/<int:session>', methods=['GET'])
-def session(name, session):
-    '''
-    GETS game session
-    '''
-    # a = Address(street1=playerId, street2=session)
-    # db.session.add(a)
-    # db.session.commit()
-
-    address = Address.query.all()
-
-    payload = '<h1>Session GET</h1><hr><p>In Development</p><hr><ul>{}</ul>'.format(str(address))
-
-    return payload 
+        return jsonify(school.json), status.HTTP_201_CREATED
+    schools = School.query.all()
+    return jsonify({'schools': unpack_json(schools)}), status.HTTP_200_OK
 
 
 # Devel Utilities
+# TODO: Create bulk creation
+
+# TODO: Remove empty or put behind password
 @app.route('/api/empty', methods=['GET'])
 def empty():
     '''
@@ -296,14 +181,17 @@ def empty():
     db.drop_all()
     db.create_all()
 
-    return 'Deleted everything in the database...'
+    return jsonify({'message':'Deleted everything in the database...'}), status.HTTP_205_RESET_CONTENT
 
 
-@app.errorhandler(404)
+def unpack_json(objects):
+    """ Serializes a list of sqlalchemy models to be ready for jsonify() """
+    return [obj.json for obj in objects]
+
+
+@app.errorhandler(status.HTTP_404_NOT_FOUND)
 def page_not_found(error):
-    # app.logger.error('Page not found: %s', (request.path))
-    # return render_template('404.html'), 404
-    return make_response(jsonify({'error': 'Not found'}), 404)
+    return jsonify({'error':'404 not found'}), status.HTTP_404_NOT_FOUND
 
 
 if __name__ == '__main__':
--- app/models.py
+++ app/models.py
@@ -1,11 +1,14 @@
+
 from sqlalchemy.orm import lazyload, raiseload, relationship
 
 from app.extensions import db
+
 
 def time_created():
     return db.Column(db.DateTime(timezone=True),
             server_default=db.func.now()
             )
+
 
 def time_updated():
     return db.Column(db.DateTime(timezone=True),
@@ -17,102 +20,65 @@ def time_updated():
 
 class Address(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-
     street1 = db.Column(db.String(64))
     street2 = db.Column(db.String(64))
     city = db.Column(db.String(64))
-    state = db.Column(db.String(32))
+    state = db.Column(db.String(2))
     zipcode = db.Column(db.String(7))
-    country = db.Column(db.String(64))
-
-    time_created = time_created()
-    time_updated = time_updated()
-
-    def __repr__(self):
-        if self.street2 is None:
-            return '< Address: {} {}, {} >'.format(self.street1, self.city, self.state)
-        return '< Address: {} {} {}, {} >'.format(self.street1, self.street2, self.city, self.state)
-
-
-class Classroom(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-
-    name = db.Column(db.String(64))
-    school_id = db.Column(db.Integer, db.ForeignKey('school.id'))
-    school = db.relationship('School')
-
+    county = db.Column(db.String(64))
     time_created = time_created()
     time_updated = time_updated()
     
+    def __repr__(self):
+        if self.county is '':
+            return '<Address: {} {}, {} {}>'.format(self.street1, self.city, self.state, self.zipcode)
+        return '<Address: {} {}, {} {} {} County>'.format(self.street1, self.city, self.state, self.zipcode, self.county)
 
-class Contact(db.Model):
+    @property
+    def json(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'id' : int(self.id),
+            'street1' : str(self.street1),
+            'street2' : str(self.street2),
+            'city' : str(self.city),
+            'state' : str(self.state),
+            'zipcode' : str(self.zipcode),
+            'county' : str(self.county),
+            }
+    
+
+class Industry(db.Model):
+    naics_code = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(512))
+    time_created = time_created()
+    time_updated = time_updated()
+    
+    def __repr__(self):
+        return '<Industry: Code {},  {}>'.format(self.naics_code, self.description)
+    
+    @property
+    def json(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'naics_code' : int(self.naics_code),
+            'description' : str(self.description),
+            }
+    
+
+class Company(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-
-    first_name = db.Column(db.String(64))
-    last_name = db.Column(db.String(64))
-    phone = db.Column(db.String(10))
-    email = db.Column(db.String(128), db.ForeignKey('login.email'))
-    login = db.relationship('Login')
+    name = db.Column(db.String(128))
     address_id = db.Column(db.Integer, db.ForeignKey('address.id'))
     address = db.relationship('Address')
-    age = db.Column(db.Integer)
-    school_id = db.Column(db.Integer, db.ForeignKey('school.id'))
-    school = db.relationship('School')
-    phone = db.Column(db.String(64))
-    role = db.Column(db.String(64))
-
+    industry_id = db.Column(db.Integer, db.ForeignKey('industry.naics_code'))
+    industry = db.relationship('Industry')
+    website = db.Column(db.String(256))
     time_created = time_created()
     time_updated = time_updated()
-
-    def __repr__(self):    
-        return '< Contact: {} {} >'.format(self.firstName, self.lastName)
-
     
-class Game(db.Model):
-    name = db.Column(db.String(64), primary_key=True)
-    subject = db.Column(db.String(64))
-    
-    time_created = time_created()
-    time_updated = time_updated()
-
     def __repr__(self):
-        return '< Game: {} >'.format(self.name)
-
-
-class Login(db.Model):
-    email = db.Column(db.String(128), primary_key=True)
-    password = db.Column(db.String(128))
-    
-    time_created = time_created()
-    time_updated = time_updated()
-
-    def __repr__(self):
-        return '< Login: {} {} >'.format(self.email, self.password)
-
-
-class Player(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-
-    name = db.Column(db.String(64))
-    grade = db.Column(db.Integer)
-    # TODO: Remove highscore - Hot fix
-    highscore = db.Column(db.Integer)
-    letter_grade = db.Column(db.String(2))
-    levels_unlocked = db.Column(db.Integer)
-    contact_id = db.Column(db.Integer, db.ForeignKey('contact.id'))
-    contact = db.relationship('Contact', foreign_keys='Player.contact_id')
-    classroom_id = db.Column(db.Integer, db.ForeignKey('classroom.id'))
-    classroom = db.relationship('Classroom')
-    guardian_id = db.Column(db.Integer, db.ForeignKey('contact.id'))
-    guardian = db.relationship('Contact', foreign_keys='Player.guardian_id')
-    game_id = db.Column(db.String(64), db.ForeignKey('game.name'))
-    game = db.relationship('Game')
-
-    time_created = time_created()
-    time_updated = time_updated()
-
-    def __repr__(self):
-        return '< Player: {} is in grade {} >'.format(self.name, self.grade)
+        return '<Company: {}>'.format(self.name)
 
     @property
     def json(self):
@@ -120,101 +86,30 @@ class Player(db.Model):
         return {
             'id' : int(self.id),
             'name' : str(self.name),
-            'grade' : str(self.grade),
-            'highscore' : int(self.highscore),
-            'letter_grade' : str(self.letter_grade),
-            'contact_id' : int(self.contact_id),
-            # 'classroom_id' : int(self.classroom_id),
+            'address_id' : int(self.address_id or 0) or None,
+            'industry_id' : int(self.industry_id or 0) or None,
+            'website' : str(self.website),
             }
 
     
-class Round(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-
-    session_id = db.Column(db.Integer, db.ForeignKey('session.id'))
-    session = db.relationship('Session')
-    level_name = db.Column(db.String(64))
-    score = db.Column(db.Integer)
-    start_time = db.Column(db.DateTime)
-    stop_time = db.Column(db.DateTime)
-    won = db.Column(db.Boolean)
-    waves_completed = db.Column(db.Integer)
-    difficulty = db.Column(db.String(64))
-    mode = db.Column(db.String(64))
-
-    time_created = time_created()
-    time_updated = time_updated()
-
-    def __repr__(self):
-        return '< Round: {} >'.format(self.id)
-
-
 class School(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-
-    name = db.Column(db.String(64))
+    name = db.Column(db.String(128))
     address_id = db.Column(db.Integer, db.ForeignKey('address.id'))
     address = db.relationship('Address')
-    district = db.Column(db.String(64))
-    style = db.Column(db.String(64))
-
-    time_created = time_created()
-    time_updated = time_updated()
-
-    def __repr__(self):
-        return '< School: {} >'.format(self.name)
-
-
-class Session(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-
-    game_id = db.Column(db.String(64), db.ForeignKey('game.name'))
-    game = db.relationship('Game')
-    game_version = db.Column(db.Integer)
-    start_time = db.Column(db.DateTime)
-    stop_time = db.Column(db.DateTime)
-    player_id = db.Column(db.Integer, db.ForeignKey('player.id'))
-    player = db.relationship('Player')
-    system_id = db.Column(db.Integer, db.ForeignKey('system.id'))
-    system = db.relationship('System')
-
-    time_created = time_created()
-    time_updated = time_updated()
-
-    def __repr__(self):
-        return '< Session: {} >'.format(self.id)
-
-    
-#class Stats(db.Model):
-#    __tablename__ = 'Stats'
-
-
-class System(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-
-    deviceId = db.Column(db.String(256))
-    deviceModel = db.Column(db.String(256))
-    operatingSystem = db.Column(db.String(256))
-    graphicsDeviceVendorId = db.Column(db.String(256))
-    graphicsDeviceId = db.Column(db.String(256))
-    graphicsDeviceVersion = db.Column(db.String(256))
-    graphicsMultiThreaded = db.Column(db.String(256))
-    graphicsShaderLevel = db.Column(db.String(256))
-    maxTextureSize = db.Column(db.String(256))
-    systemMemorySize = db.Column(db.String(256))
-    graphicsMemorySize = db.Column(db.String(256))
-    graphicsDeviceVendor = db.Column(db.String(256))
-    processorCount = db.Column(db.String(256))
-    processorType = db.Column(db.String(256))
-    supportedRenderTargetCount = db.Column(db.String(256))
-    supports2DArrayTextures = db.Column(db.String(256))
-    supports3DRenderTextures = db.Column(db.String(256))
-    supports3DTextures = db.Column(db.String(256))
-    supportsComputeShaders = db.Column(db.String(256))
-    supportsInstancing = db.Column(db.String(256))
-
+    website = db.Column(db.String(256))
     time_created = time_created()
     time_updated = time_updated()
     
     def __repr__(self):
-        return '< System: {} OS {} >'.format(self.id, self.operatingSystem)
+        return '<School: {}>'.format(self.name)
+
+    @property
+    def json(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'id' : int(self.id),
+            'name' : str(self.name),
+            'address_id' : int(self.address_id or 0) or None,
+            'website' : str(self.website),
+            }
